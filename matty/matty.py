19c42a9b937c5a484b43fa1d93adaa25da8bfd42
Sathish Jayapal <sjayapal@skminfotech.com>
2020-08-17 18:15:40 -0500

    Added functions to stop and start s3

--- matty/matty.py
+++ matty/matty.py
@@ -1,8 +1,74 @@
+from typing import List, Any
+
 import boto3
+import sys
+import click
 
 session = boto3.Session(profile_name='matty');
 ec2 = session.resource('ec2')
-for instanceData in ec2.instances.all():
-    print(instanceData)
-for vpcData in ec2.subnets.all():
-    print(vpcData)
+s3 = boto3.client('s3')
+
+
+def getInstances(project):
+    if project:
+        filters = [{'Name': 'tag:aws:cloudformation:stack-name', 'Values': ["ssathishkeypair"]}]
+        ec2instances = ec2.instances.filter(Filters=filters)
+    else:
+        ec2instances = ec2.instances.all()
+    return ec2instances
+
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+
+@instances.command('list')
+@click.option('--project', default=None, help="Only instances for project (tag Project:<name>")
+def list_instances(project):
+    """List all my EC2 instances"""
+    instances = getInstances(project)
+    for instanceData in instances:
+        print(','.join((instanceData.id, instanceData.instance_type, instanceData.state['Name'])))
+    return
+
+
+@instances.command('stop')
+@click.option('--project', default=None, help='Only stop EC2 instances')
+def stop_instances(project):
+    """Stop the EC@ instances of specified project"""
+    ec2instances = getInstances(project)
+    for oneInstance in ec2instances:
+        print("Stopping {0} ..".format(oneInstance.id))
+        oneInstance.stop()
+    return
+
+
+@instances.command('start')
+@click.option('--project', default=None, help='Only stop EC2 instances')
+def stop_instances(project):
+    """Stop the EC@ instances of specified project"""
+    ec2instances = getInstances(project)
+    for oneInstance in ec2instances:
+        print("starting {0} ..".format(oneInstance.id))
+        oneInstance.start()
+    return
+
+@click.command()
+def list_alls3():
+    """List all S3 buckets"""
+    assert isinstance(s3.list_buckets, object)
+    allBuckets = s3.list_buckets()
+    print("Comming to get the buckets")
+    for s3Instance in allBuckets['Buckets']:
+        print(s3Instance["Name"])
+
+
+def list_ec2instance_vpcs():
+    for vpcData in ec2.subnets.all():
+        print(vpcData)
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    instances()
